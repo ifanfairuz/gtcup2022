2789134d0d51a657593dbc8475f4abe200792825
Ifan Fairuz <ifanfairuz@gmail.com>
2022-09-10 21:14:42 +0700

    Update page.go

--- actions/admin/page.go
+++ actions/admin/page.go
@@ -4,8 +4,6 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
-	"strings"
-	"time"
 
 	"github.com/ifanfairuz/gtcup2022/server"
 	"github.com/ifanfairuz/gtcup2022/services"
@@ -40,16 +38,16 @@ func UpdateTeam(e echo.Context) error {
 
 func Generate(e echo.Context) error {
 	c := e.(*server.AppContext)
-	d := e.QueryParam("date")
-	start := time.Date(2022, 9, 14, 19, 0, 0, 0, time.FixedZone("Asia/Jakarta", 25200))
-	if d != "" {
-		t := strings.Split(d, "-")
-		y,_:= strconv.Atoi(t[2])
-		m,_:= strconv.Atoi(t[1])
-		d,_:= strconv.Atoi(t[0])
-		start = time.Date(y, time.Month(m), d, 19, 0, 0, 0, time.FixedZone("Asia/Jakarta", 25200))
-	}
+	// d := e.QueryParam("date")
+	// // start := time.Date(2022, 9, 14, 19, 0, 0, 0, time.FixedZone("Asia/Jakarta", 25200))
+	// if d != "" {
+	// 	// t := strings.Split(d, "-")
+	// 	// y,_:= strconv.Atoi(t[2])
+	// 	// m,_:= strconv.Atoi(t[1])
+	// 	// d,_:= strconv.Atoi(t[0])
+	// 	// start = time.Date(y, time.Month(m), d, 19, 0, 0, 0, time.FixedZone("Asia/Jakarta", 25200))
+	// }
 	matchService := services.NewMatchService(c.Server.DBM())
-	matchService.Regenerate(start, 2)
+	// matchService.Regenerate(start, 2)
 	return c.JSON(http.StatusOK, matchService.MatchRepo.All())
 }
